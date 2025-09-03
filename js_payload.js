bd8a75bed45d756edea37b634054a83b60f2f8fe
cmdkill3r <117360532+cmdkill3r@users.noreply.github.com>
2025-09-03 16:57:08 +0200

    Update js_payload.js

--- js_payload.js
+++ js_payload.js
@@ -1,5 +1,7 @@
+// Add your actual pjs payload here 
 function PAYLOAD(){
     console.println("Hello World!");
     app.setTimeOut("console.clear();", 5000);  // clear console after 5s
 }
 PAYLOAD();
+
